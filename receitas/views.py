72d6204d324d53d7e0f6b70aae0e9e1505ee25a0
Aleleonel <aleleonel@gmail.com>
2021-07-31 17:19:04 -0300

    configuração do setting, banco de dados postgres my_revenue

--- receitas/views.py
+++ receitas/views.py
@@ -3,7 +3,26 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'index.html')
+    """Receitas"""
+
+    receitas = {
+        1: 'Lazanha',
+        2: 'Caldo Verde de Ervilha',
+        3: 'Caldo de Feijão',
+        4: 'Vinho Quente',
+        5: 'Cocada Caseira',
+        6: 'Pipoca com Chocolate',
+        7: 'Bolo de Cenoura'
+    }
+
+    context = {
+        'nome_da_receita': receitas
+    }
+
+    return render(request, 'index.html', context)
+
 
 def receita(request):
+    """Paginas de receitas"""
+
     return render(request, 'receita.html')
